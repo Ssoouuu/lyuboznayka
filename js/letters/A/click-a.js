e58e27c5c32844f3329905d535848986cb510053
Ssoouuu <alinatrunova2200@gmail.com>
2026-03-17 22:53:47 +0500

    Исправлены пути для буквы А - 3

--- js/letters/A/click-a.js
+++ js/letters/A/click-a.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const images = document.querySelectorAll('.click-a__images img');
     const nextButton = document.querySelector('.btn-yellow');
 
-        nextButton.style.display = 'none';
-    
+    nextButton.style.display = 'none';
+
     let correctCount = 0;
-    
+
     images.forEach(image => {
-        image.addEventListener('click', function() {
+        image.addEventListener('click', function () {
             // Если картинку уже нажимали - выходим
             if (this.classList.contains('clicked')) return;
-            
+
             this.classList.add('clicked');
-            
+
             if (this.dataset.correct === 'true') {
                 // Правильный ответ
                 this.classList.add('correct-click');
@@ -22,11 +22,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Неправильный ответ
                 this.classList.add('wrong-click');
             }
-            
+
             // Если все 3 правильные картинки нажаты
             if (correctCount === 3) {
-            nextButton.style.display = 'inline-block';
-            nextButton.style.animation = 'pulse 0.5s ease';
+                // nextButton.style.display = 'inline-block';
+                // nextButton.style.animation = 'pulse 0.5s ease';
+                setTimeout(() => {
+                    window.location.href = '../../components-html/end.html?letter=А';
+                }, 1000);
             }
         });
     });
